cd1562c8a583855e427c5e0bb30f95c7161aec19
Kayleen Carido <kvcarido@gmail.com>
2022-06-25 23:28:08 -0700

    update roll in the DOM

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,19 @@
 const rollBtn = document.querySelector('#roll')
+const strRolls = document.querySelectorAll('.strRoll')
 
 const rollDice = () => {
-    // return Math.ceil(Math.random() * 6)
-    console.log(Math.ceil(Math.random() * 6))
+    return Math.ceil(Math.random() * 6)
+    // console.log(Math.ceil(Math.random() * 6))
 }
 
 rollBtn.addEventListener('click', rollDice)
+
+const rollStrength = () => {
+    let test = Array.from(strRolls)
+    test.forEach((el, i) => {
+        el.append(rollDice())
+    })
+
+}
+
+rollStrength()
